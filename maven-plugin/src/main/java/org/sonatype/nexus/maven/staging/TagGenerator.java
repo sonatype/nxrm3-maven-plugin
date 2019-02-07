2be413dff79ef0be54aee869924dfd753ca8f471
Nate McAfee <nmcafee@sonatype.com>
2019-02-07 14:12:40 -0500

    NEXUS-18829: Maven Staging Plugin: user readable tag generation

---
+++ maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/TagGenerator.java
@@ -0,0 +1,46 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.maven.staging;
+
+import java.text.SimpleDateFormat;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Generates a tag based on the project name, version, and current timestamp.
+ *
+ * @since 1.0.0
+ */
+@Singleton
+public class TagGenerator
+{
+  private static final String HYPHEN_DELIMITER = "-";
+
+  private static final String TIMESTAMP_PATTERN = "yyyyMMdd'T'HHmmssSSS";
+
+  private final CurrentTimeSource currentTimeSource;
+
+  @Inject
+  public TagGenerator(final CurrentTimeSource currentTimeSource) {
+    this.currentTimeSource = currentTimeSource;
+  }
+
+  public String generate(final String projectName, final String projectVersion) {
+    return String.join(HYPHEN_DELIMITER, projectName, projectVersion, getCurrentTimestamp());
+  }
+
+  private String getCurrentTimestamp() {
+    return new SimpleDateFormat(TIMESTAMP_PATTERN).format(currentTimeSource.get());
+  }
+}
