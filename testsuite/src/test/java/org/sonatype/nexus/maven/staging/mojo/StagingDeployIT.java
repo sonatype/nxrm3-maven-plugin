945b2bd9d18970c77009548a0f7442a49f2da87e
obarra-dev <obarra@sonatype.com>
2025-09-19 18:03:06 -0300

    NEXUS-48722 re getting ITs

---
+++ testsuite/src/test/java/org/sonatype/nexus/maven/staging/mojo/StagingDeployIT.java
@@ -0,0 +1,370 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.maven.staging.mojo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Map;
+
+import org.sonatype.nexus.maven.staging.test.support.StagingMavenPluginITSupport;
+
+import org.apache.maven.it.VerificationException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static java.util.UUID.randomUUID;
+import static org.apache.commons.io.FileUtils.forceDelete;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+
+public class StagingDeployIT
+    extends StagingMavenPluginITSupport
+{
+  private static final String GROUP_ID = "test.group";
+
+  private static final String VERSION = "1.0.0";
+
+  private static final String STAGING_DEPLOY = "nxrm3:staging-deploy";
+
+  private static final String DEPLOY = "deploy";
+
+  private static final String INSTALL = "install";
+
+  @Test
+  public void stagingDeploy() throws Exception {
+    assertStagingWithDeployGoal(STAGING_DEPLOY);
+  }
+
+  @Test
+  public void deploy() throws Exception {
+    assertStagingWithDeployGoal(DEPLOY);
+  }
+
+  @Test
+  public void multiModuleDeploy() throws Exception {
+    initialiseVerifier(multiModuleProjectDir);
+
+    String tag = randomUUID().toString();
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    String groupId = GROUP_ID;
+    String artifactId = randomUUID().toString();
+    String version = VERSION;
+
+    createProject(multiModuleProjectDir, RELEASE_REPOSITORY, groupId, artifactId, version);
+    createProject(project1Dir, RELEASE_REPOSITORY, groupId, artifactId, version);
+    createProject(project2Dir, RELEASE_REPOSITORY, groupId, artifactId, version);
+
+    verifier.setDebug(true);
+
+    verifier.addCliOption("-Dtag=" + tag);
+
+    verifier.executeGoals(goals);
+
+    verifyComponent(RELEASE_REPOSITORY, groupId, artifactId, version, tag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, groupId, artifactId, version);
+
+    verifyComponent(RELEASE_REPOSITORY, groupId, artifactId + "-module1", version, tag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, groupId, artifactId + "-module1", version);
+
+    verifyComponent(RELEASE_REPOSITORY, groupId, artifactId + "-module2", version, tag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, groupId, artifactId + "-module2", version);
+  }
+
+  @Test
+  public void failIfOffline() throws Exception {
+    initialiseVerifier(projectDir);
+
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    verifier.setDebug(true);
+
+    verifier.addCliOption("-Dtag=" + tag);
+    verifier.addCliOption("-o");
+
+    try {
+      verifier.executeGoals(goals);
+      Assert.fail("Expected LifecycleExecutionException");
+    }
+    catch (Exception e) {
+      assertThat(e.getMessage(),
+          containsString("Goal requires online mode for execution but Maven is currently offline"));
+    }
+  }
+
+  @Test
+  public void useExistingTag() throws Exception {
+    String tag = randomUUID().toString();
+
+    assertStagingWithDeployGoal(STAGING_DEPLOY, tag);
+
+    assertStagingWithDeployGoal(STAGING_DEPLOY, tag);
+  }
+
+  @Test
+  public void storeTagInPropertiesForNewAndExistingTag() throws Exception {
+    String tag = randomUUID().toString();
+
+    File propertiesFile = getPropertiesFile(projectDir);
+
+    assertStagingWithDeployGoal(STAGING_DEPLOY, tag);
+
+    assertThat(readFileToString(propertiesFile), containsString("staging.tag=" + tag));
+
+    forceDelete(propertiesFile);
+
+    assertStagingWithDeployGoal(STAGING_DEPLOY, tag);
+
+    assertThat(readFileToString(propertiesFile), containsString("staging.tag=" + tag));
+  }
+
+  @Test
+  public void deployWithoutSpecifyingTagUsesGeneratedTag() throws Exception {
+    initialiseVerifier(multiModuleProjectDir);
+
+    String artifactId = randomUUID().toString();
+
+    createProject(multiModuleProjectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+    createProject(project1Dir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+    createProject(project2Dir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    verifier.setDebug(true);
+
+    verifier.executeGoals(goals);
+
+    Properties properties = new Properties();
+    properties.load(new FileInputStream(getPropertiesFile(multiModuleProjectDir)));
+    String generatedTag = properties.getProperty("staging.tag");
+
+    assertThat(generatedTag, startsWith(artifactId + "-" + VERSION + "-"));
+
+    verifyComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION, generatedTag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    verifyComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId + "-module1", VERSION, generatedTag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId + "-module1", VERSION);
+
+    verifyComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId + "-module2", VERSION, generatedTag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, GROUP_ID, artifactId + "-module2", VERSION);
+  }
+
+  @Test
+  public void deployWithoutMainArtifact() throws Exception {
+    String tag = randomUUID().toString();
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add("javadoc:jar");
+    goals.add("source:jar");
+    goals.add(STAGING_DEPLOY);
+
+    ComponentItem componentItem = assertStagingWithDeployGoal(goals, tag);
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "javadoc.jar");
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "javadoc.jar.md5");
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "javadoc.jar.sha1");
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "sources.jar");
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "sources.jar.md5");
+    verifyAssetForComponent(RELEASE_REPOSITORY, componentItem, "sources.jar.sha1");
+  }
+
+  @Test
+  public void stagingDeployPomProject() throws Exception {
+    initialiseVerifier(projectDir);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    assertStagingWithDeployGoal(goals, randomUUID().toString(), POM_PACKAGING);
+  }
+
+  @Test
+  public void deployIfSkipNotSet() throws Exception {
+    initialiseVerifier(projectDir);
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    List<String> goals = new ArrayList<>();
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    deployAndVerify(goals, tag, GROUP_ID, artifactId, VERSION);
+  }
+
+  @Test
+  public void doNothingIfSkipTrue() throws Exception {
+    initialiseVerifier(projectDir);
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION, true);
+
+    List<String> goals = new ArrayList<>();
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    verifier.setDebug(true);
+
+    verifier.addCliOption("-Dtag=" + tag);
+
+    verifier.executeGoals(goals);
+
+    verifyNoComponentPresent(artifactId);
+  }
+
+  @Test
+  public void deployIfSkipFalse() throws Exception {
+    initialiseVerifier(projectDir);
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION, false);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    deployAndVerify(goals, tag, GROUP_ID, artifactId, VERSION);
+  }
+
+  @Test
+  public void deployIfCliSkipClear() throws Exception {
+    initialiseVerifier(projectDir);
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    verifier.addCliOption("-DskipNexusStagingDeployMojo=false");
+
+    deployAndVerify(goals, tag, GROUP_ID, artifactId, VERSION);
+  }
+
+  @Test
+  public void doNothingIfCliSkipSet() throws Exception {
+    initialiseVerifier(projectDir);
+    String artifactId = randomUUID().toString();
+    String tag = randomUUID().toString();
+
+    createProject(projectDir, RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add(STAGING_DEPLOY);
+
+    verifier.addCliOption("-DskipNexusStagingDeployMojo=true");
+
+    verifier.setDebug(true);
+
+    verifier.addCliOption("-Dtag=" + tag);
+
+    verifier.executeGoals(goals);
+
+    verifyNoComponentPresent(artifactId);
+  }
+
+  private void verifyNoComponentPresent(final String artifactId) throws Exception {
+    Map<String, String> searchQuery = getSearchQuery(RELEASE_REPOSITORY, GROUP_ID, artifactId, VERSION);
+    assertThat(componentSearch(searchQuery).items, hasSize(0));
+  }
+
+  private void assertStagingWithDeployGoal(final String deployGoal) throws Exception {
+    assertStagingWithDeployGoal(deployGoal, randomUUID().toString());
+  }
+
+  private void assertStagingWithDeployGoal(final String deployGoal, final String tag) throws Exception {
+    List<String> goals = new ArrayList<>();
+
+    goals.add(INSTALL);
+    goals.add("javadoc:jar");
+    goals.add(deployGoal);
+    
+    assertStagingWithDeployGoal(goals, tag);
+  }
+
+  private ComponentItem assertStagingWithDeployGoal(final List<String> goals,
+                                                    final String tag) throws Exception
+  {
+    initialiseVerifier(projectDir);
+
+    return assertStagingWithDeployGoal(goals, tag, JAR_PACKAGING);
+  }
+
+  private ComponentItem assertStagingWithDeployGoal(final List<String> goals,
+                                                    final String tag,
+                                                    final String packaging) throws Exception
+  {
+    String groupId = GROUP_ID;
+    String artifactId = randomUUID().toString();
+    String version = VERSION;
+
+    createProject(projectDir, RELEASE_REPOSITORY, groupId, artifactId, version);
+
+    return deployAndVerify(goals, tag, groupId, artifactId, version);
+  }
+
+  private ComponentItem deployAndVerify(final List<String> goals,
+                                        final String tag,
+                                        final String groupId,
+                                        final String artifactId,
+                                        final String version) throws VerificationException
+  {
+    verifier.setDebug(true);
+
+    verifier.addCliOption("-Dtag=" + tag);
+
+    verifier.executeGoals(goals);
+
+    ComponentItem componentItem = verifyComponent(RELEASE_REPOSITORY, groupId, artifactId, version, tag);
+    verifyPomAssetForComponent(RELEASE_REPOSITORY, componentItem.group, componentItem.name, componentItem.version);
+
+    return componentItem;
+  }
+
+  private File getPropertiesFile(final File projectDir) {
+    return new File(projectDir.getAbsolutePath() + "/target/nexus-staging/staging/staging.properties");
+  }
+}
---
+++ testsuite/src/test/java/org/sonatype/nexus/maven/staging/test/support/StagingMavenPluginITSupport.java
@@ -0,0 +1,205 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.maven.staging.test.support;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import javax.inject.Inject;
+
+import org.sonatype.sisu.filetasks.FileTaskBuilder;
+
+import org.apache.maven.it.Verifier;
+import org.junit.Before;
+
+import static org.apache.maven.it.util.ResourceExtractor.simpleExtractResources;
+import static org.sonatype.sisu.filetasks.builder.FileRef.file;
+
+public abstract class StagingMavenPluginITSupport
+    extends NxrmITSupport
+{
+  private static final String MAVEN_VERSION = "3.8.6";
+
+  private static final String DIRECTORY = "/maven3-project";
+
+  private static final String MULTI_MODULE_DIRECTORY = "/multi-module-maven3-project";
+
+  private static final String PROJECT_1_DIRECTORY = MULTI_MODULE_DIRECTORY + "/Proj1";
+
+  private static final String PROJECT_2_DIRECTORY = MULTI_MODULE_DIRECTORY + "/Proj2";
+
+  private static final String GROUP_ID = "org.sonatype.plugins";
+
+  private static final String ARTIFACT_ID = "nexus-staging-example-project";
+
+  private static final String VERSION = "1.0.0";
+
+  protected static final String POM_PACKAGING = "pom";
+
+  protected static final String JAR_PACKAGING = "jar";
+
+  protected static final String RELEASE_REPOSITORY = "maven-releases";
+
+  @Inject
+  private FileTaskBuilder fileTaskBuilder;
+
+  protected File projectDir;
+
+  protected File multiModuleProjectDir;
+
+  protected File project1Dir;
+
+  protected File project2Dir;
+
+  protected Verifier verifier;
+
+  @Before
+  public void setupItSupport() throws Exception {
+    projectDir = simpleExtractResources(StagingMavenPluginITSupport.class, DIRECTORY);
+
+    multiModuleProjectDir = simpleExtractResources(StagingMavenPluginITSupport.class, MULTI_MODULE_DIRECTORY);
+
+    project1Dir = simpleExtractResources(StagingMavenPluginITSupport.class, PROJECT_1_DIRECTORY);
+
+    project2Dir = simpleExtractResources(StagingMavenPluginITSupport.class, PROJECT_2_DIRECTORY);
+  }
+
+  protected void createProject(final File dir,
+                               final String repository,
+                               final String groupId,
+                               final String artifactId,
+                               final String version) {
+    createProject(dir, repository, groupId, artifactId, version, JAR_PACKAGING);
+  }
+
+  protected void createProject(final File dir,
+                               final String repository,
+                               final String groupId,
+                               final String artifactId,
+                               final String version,
+                               final String packaging)
+  {
+    final File pom = new File(dir, "pom.xml");
+    final File rawPom = new File(dir, "raw-pom.xml");
+
+    final Properties properties = getDefaultProperties(repository, groupId, artifactId, version, packaging);
+
+    fileTaskBuilder.copy().file(file(rawPom)).filterUsing(properties).to().file(file(pom)).run();
+  }
+
+  protected void createProject(final File dir,
+                               final String repository,
+                               final String groupId,
+                               final String artifactId,
+                               final String version,
+                               final boolean skip)
+  {
+    final File pom = new File(dir, "pom.xml");
+    final File rawPom = new File(dir, "raw-pom-with-skip.xml");
+
+    final Properties properties = getDefaultProperties(repository, groupId, artifactId, version, JAR_PACKAGING);
+    properties.setProperty("nexus.skipNexusStagingDeployMojo", skip ? "true" : "false");
+
+    fileTaskBuilder.copy().file(file(rawPom)).filterUsing(properties).to().file(file(pom)).run();
+  }
+
+  protected void createProject(final File dir,
+                               final String repository,
+                               final String groupId,
+                               final String artifactId,
+                               final String version,
+                               final String sourceRepository,
+                               final String targetRepository)
+  {
+    final File pom = new File(dir, "pom.xml");
+    final File rawPom = new File(dir, "raw-pom-with-move.xml");
+
+    final Properties properties = getDefaultProperties(repository, groupId, artifactId, version, JAR_PACKAGING);
+    properties.setProperty("nexus.destinationRepository", targetRepository);
+    properties.setProperty("nexus.sourceRepository", sourceRepository);
+
+    fileTaskBuilder.copy().file(file(rawPom)).filterUsing(properties).to().file(file(pom)).run();
+  }
+
+  private Properties getDefaultProperties(final String repository,
+                                          final String groupId,
+                                          final String artifactId,
+                                          final String version,
+                                          final String packaging)
+  {
+    final Properties properties = new Properties();
+    properties.setProperty("nexus.url", "http://localhost:" + getPort());
+    properties.setProperty("nexus.repository", repository);
+    properties.setProperty("test.project.groupId", groupId);
+    properties.setProperty("test.project.artifactId", artifactId);
+    properties.setProperty("test.project.version", version);
+    properties.setProperty("test.project.packaging", packaging);
+    return properties;
+  }
+
+  protected void initialiseVerifier(final File projectRoot) throws Exception {
+    String settingsXml = new File(projectRoot, "preset-nexus-maven-settings.xml").getAbsolutePath();
+
+    Path mvnwHome = getMvnwHome();
+    if (mvnwHome != null) {
+      System.setProperty("maven.home", mvnwHome.toAbsolutePath().toString());
+    }
+
+    verifier = new Verifier(projectRoot.getAbsolutePath(), settingsXml);
+
+    verifier.setMavenDebug(true);
+
+    // Cleanup the artifact in case it was previously built
+    verifier.deleteArtifact(GROUP_ID, ARTIFACT_ID, VERSION, POM_PACKAGING);
+
+    List<String> options = new ArrayList<>();
+    options.add("-Djava.awt.headless=true"); // on Mac a Dock icon bumps on every Verifier invocation
+    options.add("-gs " + settingsXml); // Override the global settings to allow local/user settings to be respected
+    verifier.setCliOptions(options);
+  }
+
+  private Path getMvnwHome() {
+    String mavenUserHome = System.getProperty("maven.user.home");
+    String envMavenUserHome = System.getProperty("MAVEN_USER_HOME");
+
+    final Path mavenHome;
+    if (mavenUserHome != null) {
+      mavenHome = Paths.get(mavenUserHome);
+    }
+    else if (envMavenUserHome != null) {
+      mavenHome = Paths.get(envMavenUserHome);
+    }
+    else {
+      mavenHome = Paths.get(System.getProperty("user.home")).resolve(".m2");
+    }
+    try {
+      Path wrappedMavenHome = Files.find(
+          mavenHome.resolve("wrapper"),
+          5,
+          (path, basicFileAttributes) ->
+              path.getFileName().toString().equals("apache-maven-" + MAVEN_VERSION) && Files.exists(path.resolve("bin"))
+      ).findFirst().orElse(null);
+
+      return wrappedMavenHome;
+    }
+    catch (Exception e) {
+      System.out.println("Unable to locate maven wrapper directory");
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
